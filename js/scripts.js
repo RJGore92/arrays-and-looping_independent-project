645da090329dfa0d1ee1af0406165075ac65bb41
Robert Gore <DarkburstGX@gmail.com>
2019-02-06 11:26:02 -0800

    Initial commit for Beep Boop project on Epicodus's Unit Three Independent Project.  This commit contains in-progress HTML, CSS, JS, and an image set of the code writer for the project.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function onClickOne() {
+  $("div#intro-div-one").slideToggle();
+  $("div#intro-div-two").slideToggle();
+}
+
+function onClickTwo() {
+  $("div#intro-div-two").slideToggle();
+  $("div#primary-program-div").slideToggle();
+}
