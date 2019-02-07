c395d44381b2aff92fe81a70203bdf1a15f5c2fb
Robert Gore <DarkburstGX@gmail.com>
2019-02-07 13:38:34 -0800

    bugfix commit.  Code author has fixed issues regarding numbers where high numbers such as 400 or 500 ranges have their appropriate specs met within the program return.

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,10 @@ function converterOfNumbers(inputArray) {
           continue checkToConvert;
         }
         else if (numberSplit[j] == 0) {
+          if (numberSplit[j+1] == 1 && j != numberSplit.length) {
+            postConvertedArray.push("Boop!");
+            continue checkToConvert;
+          }
           postConvertedArray.push("Beep!");
           continue checkToConvert;
         }
